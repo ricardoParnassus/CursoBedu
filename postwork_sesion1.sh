1a8c52c3bfb4c8029faf4c1958ea7f9edceec03f
ricardoParnassus <ricardogarciaperez@hotmail.com>
2020-03-01 23:51:29 -0600

    x

---
+++ postwork_sesion1.sh
@@ -0,0 +1,13 @@
+cd Documents
+mkdir MiProyecto
+ls
+cd MiProyecto
+curl "http://files.grouplens.org/datasets/movielens/ml-1m.zip" > "C:\Users\Phrankie Garcia\Documents\MiProyecto\ml-1M.zip"
+unzip ml-1M.zip
+rm ml-1M.zip
+ls
+cd ..
+ls
+cp -a MiProyecto MiProyecto.1
+ls
+
